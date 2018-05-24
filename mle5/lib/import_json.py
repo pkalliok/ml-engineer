76eb5d83824db2991bdbd9c4417edd5526c362f1
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2018-05-24 09:34:31 +0300

    Parquet file import

--- mle5/lib/import_json.py
+++ mle5/lib/import_json.py
@@ -6,17 +6,23 @@ import pandas as pd
 
 db_url = 'postgresql+psycopg2://postgres@localhost:15432/postgres'
 
-def import_json_to_sql(filename):
-    e = create_engine(db_url)
-    tablename = splitext(filename)[0]
-    df = pd.read_json(open(filename))
+def read_parquet(filename):
+    from fastparquet import ParquetFile as pf
+    return pf(filename).to_pandas()
+
+def import_to_sql(df, e, tablename):
     rows, cols = df.shape
     if rows < cols: df = df.transpose()
     df.to_sql(tablename, e)
 
+def import_file_to_sql(filename):
+    df = read_parquet(filename) if filename.endswith('.parquet') \
+            else pd.read_json(open(filename))
+    import_to_sql(df, create_engine(db_url), splitext(filename)[0])
+
 if __name__ == '__main__':
     import sys
-    import_json_to_sql(sys.argv[1])
+    import_file_to_sql(sys.argv[1])
 
 #e.execute('SELECT 1').fetchall()[(1,)]
 
